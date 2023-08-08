f7704cd6c90ae3f1b560304198302f17bec34d89
EspeditoJunior <e.jr42@hotmail.com>
2023-08-08 02:02:00 -0300

    trabalando com express

--- src/app.js
+++ src/app.js
@@ -3,19 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get('', (req,res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather!</h1>')
 })
 
 app.get('/help', (req,res) => {
-    res.send('Help page')
+    res.send([{
+        name: "Andrew",
+        age: 27
+    },{
+        name: "Andrew!",
+        age: 30
+    }])
 })
 
 app.get('/about', (req,res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req,res) => {
-    res.send('Your wather')
+    res.send({forecast: 'Sunny', location: 'Sao Paulo'})
 })
 
 app.listen(3000, () => {
