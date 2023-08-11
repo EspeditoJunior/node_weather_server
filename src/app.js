15f3ce6c1cecd539dd2f02e87ed43389abb344a3
EspeditoJunior <e.jr42@hotmail.com>
2023-08-11 02:34:05 -0300

    handlebars

--- src/app.js
+++ src/app.js
@@ -6,7 +6,28 @@ console.log(path.join(__dirname,'../public'))
 
 const app = express()
 
+app.set('view engine','hbs')
 app.use(express.static(path.join(__dirname,'../public')))
+
+
+app.get('', (req,res) => {
+    res.render('index',{
+        title: 'Weather app',
+        name: 'Espedito'
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about',{
+        about: 'About!',
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        message: 'Help message',
+    })
+})
 
 app.get('/weather', (req,res) => {
     res.send({forecast: 'Sunny', location: 'Sao Paulo'})
