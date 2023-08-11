be53d3dcb655fc43079403e9df13cdcfbb52fbfd
EspeditoJunior <e.jr42@hotmail.com>
2023-08-11 02:13:15 -0300

    express serving static content

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
+const path = require('path')
 const express = require('express')
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
 
 const app = express()
 
-app.get('', (req,res) => {
-    res.send('<h1>Weather!</h1>')
-})
-
-app.get('/help', (req,res) => {
-    res.send([{
-        name: "Andrew",
-        age: 27
-    },{
-        name: "Andrew!",
-        age: 30
-    }])
-})
-
-app.get('/about', (req,res) => {
-    res.send('<h1>About page</h1>')
-})
+app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('/weather', (req,res) => {
     res.send({forecast: 'Sunny', location: 'Sao Paulo'})
